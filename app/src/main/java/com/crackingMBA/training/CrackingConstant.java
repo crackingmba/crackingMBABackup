690836a534070eb1fc0995aae7588917ec3d0d66
thecrackingmba <sampleproject123>
2017-03-01 18:55:30 +0530

    Home and Prepariton Sections new service migration

--- app/src/main/java/com/crackingMBA/training/CrackingConstant.java
+++ app/src/main/java/com/crackingMBA/training/CrackingConstant.java
@@ -14,11 +14,11 @@ public class CrackingConstant {
     public static final String QUAT_CODE="quant";
     public static final String VERBAL_CODE="verbal";
     public static final String DILR_CODE="dilr";
-    public static final String HOME_TAB_GETVIDEOLIST__QUANT_SERVICE_URL="http://www.crackingmba.com/getVideoList.php?category=quant&subcategory=recent";
+    public static final String HOME_TAB_GETVIDEOLIST
+    public static final String GETVIDEOLIST_SERVICE_URL__QUANT_SERVICE_URL="http://www.crackingmba.com/getVideoList.php?category=quant&subcategory=recent";
     public static final String HOME_TAB_GETVIDEOLIST__DILR_SERVICE_URL="http://www.crackingmba.com/getVideoList.php?category=dilr&subcategory=recent";
     public static final String HOME_TAB_GETVIDEOLIST__VERBAL_SERVICE_URL="http://www.crackingmba.com/getVideoList.php?category=verbal&subcategory=recent";
 
-    public static final String GETVIDEOLIST_SERVICE_URL="http://www.crackingmba.com/getVideoList.php";
     public static final String DEVELOPER_KEY = "AIzaSyAlp1g-Ywycw2hqEJnpoU73NHLBF9akfJo";
 
     public static final String GET_MOCKTEST_TOPICS_SERVICE_URL="http://crackingmba.com/getMTSubCategories.php";
