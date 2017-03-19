cf4e2786d74788bbcbb40a577a0307220294508c
thecrackingmba <thecrackingmba@gmail.com>
2017-03-19 10:45:50 +0530

    Vijender removed the deprecated Methods from HomeFragment

--- app/src/main/java/com/crackingMBA/training/HomeFragment.java
+++ app/src/main/java/com/crackingMBA/training/HomeFragment.java
@@ -72,7 +72,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 // Suresh Added comments
         //Vijender comment added
         //added one more comments by Suresh
-
+        //added one new comment by Vijender
 
         rootView = inflater.inflate(R.layout.fragment_home1, container, false);
         gotoquantBtn = (Button) rootView.findViewById(R.id.gotoquant3);
