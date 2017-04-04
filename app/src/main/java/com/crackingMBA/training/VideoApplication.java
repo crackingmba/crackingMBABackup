f7ce9c61d885ba123440326ac08a2941bf6677f4
Harish523 <chharishkumarjnv@gmail.com>
2017-04-04 22:34:21 +0530

    Committing Change for forcing Portrait mode

--- app/src/main/java/com/crackingMBA/training/VideoApplication.java
+++ app/src/main/java/com/crackingMBA/training/VideoApplication.java
@@ -1,6 +1,9 @@
 package com.crackingMBA.training;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 import com.crackingMBA.training.pojo.MockTestQuestion;
 import com.crackingMBA.training.pojo.MockTestTest;
@@ -51,6 +54,49 @@ public class VideoApplication extends Application {
     public void onCreate() {
         super.onCreate();
         this.currentInstance = this;
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            @Override
+            public void onActivityCreated(Activity activity,
+                                          Bundle savedInstanceState) {
+
+                // Force activity's orientation to portrait
+                activity.setRequestedOrientation(
+                        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+        });
     }
 
     public  static  VideoApplication getInstance(){
