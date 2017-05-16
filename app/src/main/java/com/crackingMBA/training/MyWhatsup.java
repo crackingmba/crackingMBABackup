02fb4efbb0ebb79d76a107fd7c9f40ab1a94a9ca
thecrackingmba <thecrackingmba@gmail.com>
2017-05-16 14:01:51 +0530

    Updated the layout and functionality for Home and Prep Tabs

--- app/src/main/java/com/crackingMBA/training/MyWhatsup.java
+++ app/src/main/java/com/crackingMBA/training/MyWhatsup.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
@@ -19,12 +20,17 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.crackingMBA.training.pojo.LoginResponseObject;
 import com.crackingMBA.training.pojo.Question;
+import com.crackingMBA.training.util.HttpHandler;
 import com.facebook.CallbackManager;
 import com.facebook.login.LoginManager;
 import com.facebook.login.widget.LoginButton;
@@ -43,7 +49,12 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -54,16 +65,85 @@ public class MyWhatsup extends Fragment implements View.OnClickListener {
     View rootView;
     TextView textView;
 
+    private String TAG = MyWhatsup.class.getSimpleName();
+    private ListView lv;
+    ArrayList<HashMap<String, String>> contactList;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_whatsup, container, false);
-        textView=(TextView)rootView.findViewById(R.id.whatsup_text);
-        textView.setText(Html.fromHtml("<h2>What's new on crackingMBA!</h2>" +
-                "<h3>15th May</h3>" +"<p>1. Added a new video on Sentence Completion in Verbal Section</p>"+"<p>2. Added a new mock test on Sentence Completion in Verbal section</p>"
-        +"<h3>14th May</h3><p>1. Added a new video on Ratio in Quant Section</p><p>2. Added a new video on Bar Charts in DI LR section</p>"));
 
+        contactList = new ArrayList<>();
+        lv = (ListView) rootView.findViewById(R.id.list);
+
+        new GetContacts().execute();
         return rootView;
+    }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(rootView.getContext(),"sample text", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            ListAdapter adapter = new SimpleAdapter(rootView.getContext(), contactList,
+                    R.layout.fragment_whatsup_row_layout, new String[]{ "day_name","update"},
+                    new int[]{R.id.day_name, R.id.update});
+            lv.setAdapter(adapter);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "http://www.crackingmba.com/getWhatsUpList.php";
+            String jsonStr = sh.makeServiceCall(url);
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try{
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    // Getting JSON Array node
+                    JSONArray contacts = jsonObj.getJSONArray("whatsup");
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        String id = c.getString("id");
+                        String day_name = c.getString("day_name");
+                        String update = c.getString("update"); //updated section
+
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("day_name", day_name);
+                        contact.put("update", update);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+
+
+
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            return null;
+        }
     }
 
 
