97c59690529434c60a407ab1da78063d5b665cda
thecrackingmba <thecrackingmba@gmail.com>
2017-09-02 17:50:38 +0530

    Updated the layout and functionality for Home and Prep Tabs

--- app/src/main/java/com/crackingMBA/training/HomeFragment.java
+++ app/src/main/java/com/crackingMBA/training/HomeFragment.java
@@ -1,14 +1,19 @@
 package com.crackingMBA.training;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,8 +30,11 @@ import com.crackingMBA.training.pojo.RetrofitNoticeBoardList;
 import com.crackingMBA.training.restAPI.ForumCommentsAPIService;
 import com.crackingMBA.training.restAPI.NoticeBoardAPIService;
 import com.crackingMBA.training.restAPI.RestClient;
+import com.crackingMBA.training.util.FileDownloader;
 import com.crackingMBA.training.util.MyUtil;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -86,9 +94,21 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
             public void onClick(View view) {
                 switch (view.getId()){
                     case R.id.online_sessions_layout:{
-                        Intent examDetails = new Intent(getActivity(), ExamDetailsActivity.class);
-                        examDetails.putExtra("MBA_EXAM_CODE", "SESSIONS");
-                        startActivity(examDetails);
+                        //Intent examDetails = new Intent(getActivity(), ExamDetailsActivity.class);
+                        //examDetails.putExtra("MBA_EXAM_CODE", "SESSIONS");
+                        //startActivity(examDetails);
+                        new DownloadFile().execute("http://maven.apache.org/maven-1.x/maven.pdf", "maven.pdf");
+                        File pdfFile = new File(Environment.getExternalStorageDirectory() + "/testthreepdf/" + "maven.pdf");  // -> filename = maven.pdf
+                        Uri path = Uri.fromFile(pdfFile);
+                        Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
+                        pdfIntent.setDataAndType(path, "application/pdf");
+                        pdfIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                        try{
+                            startActivity(pdfIntent);
+                        }catch(ActivityNotFoundException e){
+                            Toast.makeText(getContext(), "No Application available to view PDF", Toast.LENGTH_SHORT).show();
+                        }
                         break;
                     }
                     case R.id.study_material_layout:{
@@ -170,7 +190,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
             }
         };
 
-        //online_sessions_layout.setOnClickListener(examOnClickListener);
+        online_sessions_layout.setOnClickListener(examOnClickListener);
         //study_material_layout.setOnClickListener(examOnClickListener);
         //mock_tests_layout.setOnClickListener(examOnClickListener);
         //videos_layout.setOnClickListener(examOnClickListener);
@@ -226,6 +246,32 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
 
     }
+
+    private class DownloadFile extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            String fileUrl = strings[0];   // -> http://maven.apache.org/maven-1.x/maven.pdf
+            String fileName = strings[1];  // -> maven.pdf
+            String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
+            File folder = new File(extStorageDirectory, "testthreepdf");
+            folder.mkdir();
+
+            File pdfFile = new File(folder, fileName);
+
+            try{
+                pdfFile.createNewFile();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            FileDownloader.downloadFile(fileUrl, pdfFile);
+            return null;
+        }
+    }
+
+
+
+
 
     @Override
     public void onResume() {
